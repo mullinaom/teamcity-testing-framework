f08aae8e0f6c2790a5ae12d091de6ffc0acfba34
Oksana <oksanochkamullina@gmail.com>
2024-10-06 12:06:50 +0300

    added TestDataStorage class to clear generated test data

--- src/test/java/com/example/teamcity/api/BaseTest.java
+++ src/test/java/com/example/teamcity/api/BaseTest.java
@@ -1,11 +1,12 @@
 package com.example.teamcity.api;
 
+import com.example.teamcity.api.generators.TestDataStorage;
+import com.example.teamcity.api.models.TestData;
 import com.example.teamcity.api.requests.CheckedRequests;
 import com.example.teamcity.api.spec.Specifications;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.asserts.SoftAssert;
-import com.example.teamcity.api.models.TestData;
 
 import static com.example.teamcity.api.generators.TestDataGenerator.generate;
 
@@ -23,5 +24,6 @@ public class BaseTest {
     @AfterMethod(alwaysRun = true)
     public void afterTest() {
         softy.assertAll();
+        TestDataStorage.getStorage().deleteCreatedEntities();
     }
 }
